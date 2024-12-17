200bc67da3cff0612a16543134ee379a8c256b01
chawklyartun101423 <chawklyartun23@gmail.com>
2024-12-17 06:44:43 +0630

    Add project files.

---
+++ WebAPI_JWT/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI_JWT.Models
+{
+    public class User
+    {
+        public string Username {  get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+
+
+    }
+}
